c5bd96a3c21d34ad8a0382c1c2811b34fdae3fed
xiongzijie01370 <xiongzijie@smyfinancial.com>
2022-06-24 23:51:54 +0800

    return and handle an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,15 @@
 package greetings
 
 import "fmt"
+import "errors"
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("name is empty!")
+	}
+
 	// 将字符串赋值给变量 message
 	message := fmt.Sprintf("Hi, %v. Welcome !", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"connor/greetings"
+	"log"
 )
 
 func main() {
-	// 调用 Hello 方法，返回 message
-	message := greetings.Hello("Connor")
+	// 给Log预定义一些属性，比如输出前缀和禁止打印时间、源文件、行号的标识
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	// 调用 Hello 方法，返回 message, err
+	message, err := greetings.Hello("")
+	// 如果err不为nul，则表示有错误发生，将错误信息打印出来，并且退出程序
+	if err != nil {
+		log.Fatal(err)
+	}
 	// 打印 message 信息
 	print(message)
 }
